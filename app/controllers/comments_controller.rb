59fb3a892b3b08b635f313a4a7b3e5ebdbc10e2d
toptier <toptier@toptiers-Mac-mini-2.local>
2016-04-18 10:59:54 -0300

    index books and index users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,20 @@
 class CommentsController < ApplicationController
-	skip_before_filter :verify_authenticity_token, only: [:create]
-	before_filter :load_book, only: :create
-	before_action :authenticate_user!, :only => [:create]
+  skip_before_filter :verify_authenticity_token, only: [:create]
+  before_filter :load_book, only: :create
+  before_action :authenticate_user!, :only => [:create]
 
+  def create
+    @book.comments.create(comments_params)
+    redirect_to :back
+  end
 
+  private
 
-	def create
-		puts comments_params 
-		@book.comments.create(comments_params)
-		
-		redirect_to :back 
-		#respond_to do |format|
-		#format.html 
-     	#format.js { render :create, layout: false }
-    	
-	end
+  def comments_params
+    params.require(:comment).permit(:stars, :text).merge(user_id: current_user.id)
+  end
 
-	private 
-
-	def comments_params
-		params.require(:comment).permit(:stars, :text).merge(user_id: current_user.id)
-	end
-
-	def load_book
-		@book = Book.find(params[:book_id])
-	end
+  def load_book
+    @book = Book.find(params[:book_id])
+  end
 end
