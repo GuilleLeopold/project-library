8c9eec06cebee0eddc51b6cdea5e1dda1c70342a
toptier <toptier@toptiers-Mac-mini-2.local>
2016-04-20 14:18:38 -0300

    Some final fixes

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -1,15 +1,18 @@
 ActiveAdmin.register Book do
 
-
     form do |f|
     inputs 'Details' do
       input :image
+      input :title
+      input :description
+      input :year
+      input :ISBN
       actions
     end
 
   end
 
-  permit_params :image
+  permit_params :image, :title, :ISBN, :year, :description
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
